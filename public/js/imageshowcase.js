582cc80ca65569d24a33d3b08c192d6f612a0bae
TechS2105 <techsovan.github0521@gmail.com>
2025-03-30 14:02:11 +0530

    modify imageshowcase.js imageshowcase.css and imageshowcase.ejs

--- public/js/imageshowcase.js
+++ public/js/imageshowcase.js
@@ -1,4 +1,4 @@
-let closeImageContainer = document.querySelector(".bigimagecontainer");
+let closeImageContainer = document.querySelectorAll(".bigimagecontainer");
 let firstImages = document.querySelectorAll(".firstimagebox .galleryimage");
 let secondImages = document.querySelectorAll(".secondimagebox .galleryimage");
 let thirdImages = document.querySelectorAll(".thirdimagebox .galleryimage");
@@ -6,6 +6,8 @@ let imageText = document.querySelector(".imagetext");
 let imageTextLogo = document.querySelector(".imagetext .logoimg");
 let imageSubText = document.querySelector(".imagetext .textandlink h2");
 let imageSubLink = document.querySelector(".imagetext .textandlink a");
+let bigImageContainer = document.querySelectorAll(".bigimagecontainer");
+let bigImageContainerImage = document.querySelectorAll(".bigimagecontainer .bigimage img");
 
 firstImages[0].style.transform = "scale(0)";
 firstImages[0].style.filter = "blur(10px)";
@@ -30,7 +32,20 @@ imageSubLink.style.transform = "translateY(500px)";
 
 function imageCloseFun() {
 
-    closeImageContainer.style.display = "none";
+    closeImageContainer[0].style.transform = "scale(0)";
+    closeImageContainer[0].style.opacity = "0";
+
+    closeImageContainer[1].style.transform = "scale(0)";
+    closeImageContainer[1].style.opacity = "0";
+
+    closeImageContainer[2].style.transform = "scale(0)";
+    closeImageContainer[2].style.opacity = "0";
+
+    closeImageContainer[3].style.transform = "scale(0)";
+    closeImageContainer[3].style.opacity = "0";
+
+    closeImageContainer[4].style.transform = "scale(0)";
+    closeImageContainer[4].style.opacity = "0";
 
 }
 
@@ -122,11 +137,84 @@ function imageShowcaseAnimation() {
 
 }
 
+bigImageContainer.forEach((image) => {
+
+    image.style.transform = "scale(0)";
+
+});
+
+// use arrow function 
+function bigImage (){
+
+    firstImages[0].addEventListener("click", () => {
+
+        bigImageContainerImage[0].setAttribute(
+            "src",
+            `https://images.pexels.com/photos/14289046/pexels-photo-14289046.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2`
+        );
+        bigImageContainer[0].style.transform = "scale(1)";
+        bigImageContainer[0].style.transition = "all 0.7s ease";
+        bigImageContainer[0].style.opacity = "1";
+
+    });
+
+    firstImages[2].addEventListener("click", () => {
+
+        bigImageContainerImage[1].setAttribute(
+          "src",
+          `https://images.pexels.com/photos/13948622/pexels-photo-13948622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2`
+        );
+        bigImageContainer[1].style.transform = "scale(1)";
+        bigImageContainer[1].style.transition = "all 0.7s ease";
+        bigImageContainer[1].style.opacity = "1";
+
+    });
+
+    secondImages[1].addEventListener("click", () => {
+
+        bigImageContainerImage[2].setAttribute(
+            "src",
+            `https://images.pexels.com/photos/9938557/pexels-photo-9938557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2`
+        );
+
+        bigImageContainer[2].style.transform = "scale(1)";
+        bigImageContainer[2].style.transition = "all 0.7s ease";
+        bigImageContainer[2].style.opacity = "1";
+
+    });
+
+    thirdImages[0].addEventListener("click", () => {
+
+        bigImageContainerImage[3].setAttribute(
+            "src",
+            `https://images.pexels.com/photos/15873616/pexels-photo-15873616/free-photo-of-a-colorful-durga-goddess-statue.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2`
+        );
+        bigImageContainer[3].style.transform = "scale(1)";
+        bigImageContainer[3].style.transition = "all 0.7s ease";
+        bigImageContainer[3].style.opacity = "1";
+
+    });
+
+    thirdImages[2].addEventListener("click", () => {
+
+        bigImageContainerImage[4].setAttribute(
+          "src",
+          `https://images.pexels.com/photos/19241118/pexels-photo-19241118/free-photo-of-golden-durga-statue.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2`
+        );
+        bigImageContainer[4].style.transform = "scale(1)";
+        bigImageContainer[4].style.transition = "all 0.7s ease";
+        bigImageContainer[4].style.opacity = "1";
+
+    })
+
+}
+
 window.addEventListener("load", () => {
 
     setTimeout(() => {
 
         imageShowcaseAnimation();
+        bigImage();
 
     });
 
