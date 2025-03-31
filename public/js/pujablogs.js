eb07139a8d914904a1d77bf8414677cb5dd794d0
TechS2105 <techsovan.github0521@gmail.com>
2025-04-01 01:02:10 +0530

    modify pujablogs.js, pujsblogs.css and pujablogs.ejs file

---
+++ public/js/pujablogs.js
@@ -0,0 +1,17 @@
+let currentMonthandYear = document.querySelectorAll(
+  ".blogcarousel .blogbox .blogtext .blogtext1 .blogcontent2 p"
+);
+
+const currentDate = new Date();
+const f = new Intl.DateTimeFormat("en-us", 
+
+    {
+
+        dateStyle: "full"
+
+    }
+
+)
+
+
+
