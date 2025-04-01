e9794c91e8e067699ab3b6415779a622418284e1
TechS2105 <techsovan.github0521@gmail.com>
2025-04-02 00:22:48 +0530

    add blogpersondetails.js and modify index.ejs file

---
+++ views/blogpersondetails.js
@@ -0,0 +1,16 @@
+const currentDate = new Date().toDateString();
+
+const blogPersonDetails = [
+
+    {
+    
+        id: "1",
+        name: "Rahul Roy",
+        image: "",
+        date: `${currentDate}`
+    
+    }
+
+]
+
+export default blogPersonDetails;
