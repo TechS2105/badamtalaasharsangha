e3c49f8f5b3d16881513b4be1ad970beb4fa65e2
TechS2105 <techsovan.github0521@gmail.com>
2025-04-01 21:26:21 +0530

    modify pujablogs.js, footer.css, pujsblogs.css and pujablogs.ejs file

--- public/js/pujablogs.js
+++ public/js/pujablogs.js
@@ -1,17 +1,104 @@
-let currentMonthandYear = document.querySelectorAll(
-  ".blogcarousel .blogbox .blogtext .blogtext1 .blogcontent2 p"
-);
+let pujaBlogHeadingBackground = document.querySelector(".blogsectioncoloroverlay .blogtext");
+let pujaBlogHeading = document.querySelector(".blogsectioncoloroverlay .blogtext h2");
+let blogCarouselSection = document.querySelector(".blogsection");
+let blogCarouselBox = document.querySelectorAll(".blogcarousel");
 
-const currentDate = new Date();
-const f = new Intl.DateTimeFormat("en-us", 
+blogCarouselBox.forEach((box) => {
 
-    {
+  box.style.opacity = "0";
 
-        dateStyle: "full"
+});
+
+$(pujaBlogHeadingBackground).css({
+
+  opacity: "0"
+
+});
+
+$(pujaBlogHeading).css({
+
+  opacity: "0",
+  transform: "translateY(500px)"
+
+});
+
+function blogHeading() {
+  
+  $(window).on("scroll", () => {
+
+    if ($(window).scrollTop() > 5400) {
+      
+      $(pujaBlogHeadingBackground).css({
+
+        opacity: "1",
+        transition: "all 0.7s ease"
+
+      });
+
+      $(pujaBlogHeading).css({
+        opacity: "1",
+        transform: "translateY(0px)",
+        transition: "all 0.7s ease",
+      });
+
+    } else {
+      
+      $(pujaBlogHeadingBackground).css({
+
+        opacity: "0",
+        transition: "all 0.7s ease 0.5s"
+
+      });
+
+      $(pujaBlogHeading).css({
+
+        opacity: "0",
+        transform: "translateY(500px)",
+        transition: "all 1s ease 0.6s"
+
+      })
 
     }
 
-)
+  });
 
+}
 
+function blogBoxCarousel() {
+  
+  $(window).on("scroll", () => {
 
+    if ($(window).scrollTop() > 5400) {
+      
+      blogCarouselBox.forEach((box) => {
+
+        box.style.opacity = "1";
+        box.style.transition = "all 2s ease 0.5s";
+
+      });
+
+    } else {
+      
+      blogCarouselBox.forEach((box) => {
+
+        box.style.opacity = "0";
+        box.style.transition = "all 2s ease";
+
+      });
+
+    }
+
+  });
+
+}
+
+$(window).on("load", () => {
+
+  setTimeout(() => {
+
+    blogHeading();
+    blogBoxCarousel();
+
+  });
+
+})
